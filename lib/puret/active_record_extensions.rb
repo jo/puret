959fa30213a8d9073ebee7c1ecc5750d07fa4747
Roger Campos <roger@itnig.net>
2011-02-22 18:02:34 +0100

    the fallback translation chosen must have the given attribute

--- lib/puret/active_record_extensions.rb
+++ lib/puret/active_record_extensions.rb
@@ -36,8 +36,8 @@ module Puret
             # if translation not present in current locale,
             # use default locale, if present.
             # Otherwise use first translation
-            translation = translations.detect { |t| t.locale.to_sym == I18n.locale } ||
-              translations.detect { |t| t.locale.to_sym == puret_default_locale } ||
+            translation = translations.detect { |t| t.locale.to_sym == I18n.locale && t[attribute] } ||
+              translations.detect { |t| t.locale.to_sym == puret_default_locale && t[attribute] } ||
               translations.first
 
             translation ? translation[attribute] : nil
